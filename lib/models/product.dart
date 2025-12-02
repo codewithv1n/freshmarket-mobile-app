ee241f2a25889aa8f08ea919b6c166a1ad22fcef
codewithv1n <vinzelmarano@gmail.com>
2025-12-02 13:54:41 +0800

    UPDATE: new file uploaded

---
+++ lib/models/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String name;
+  final String category;
+  final double price;
+  final String unit;
+  final String image;
+
+  Product({
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.unit,
+    required this.image,
+  });
+}
