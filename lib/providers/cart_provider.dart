f218857db5f277a5e33cd386255e78752e37357b
codewithv1n <vinzelmarano@gmail.com>
2025-12-06 08:59:34 +0800

    UPDATE: new file uploaded

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../models/cart_item.dart';
+import '../models/product.dart';
+
+class CartProvider extends ChangeNotifier {
+  final List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
+
+  int get itemCount => _items.fold(0, (sum, item) => sum + item.quantity);
+
+  double get totalPrice =>
+      _items.fold(0, (sum, item) => sum + (item.product.price * item.quantity));
+
+  void addToCart(Product product) {
+    final existingIndex =
+        _items.indexWhere((item) => item.product.name == product.name);
+    if (existingIndex >= 0) {
+      _items[existingIndex].quantity++;
+    } else {
+      _items.add(CartItem(product: product));
+    }
+    notifyListeners();
+  }
+
+  void removeFromCart(Product product) {
+    _items.removeWhere((item) => item.product.name == product.name);
+    notifyListeners();
+  }
+
+  void updateQuantity(Product product, int quantity) {
+    final index =
+        _items.indexWhere((item) => item.product.name == product.name);
+    if (index >= 0) {
+      if (quantity <= 0) {
+        _items.removeAt(index);
+      } else {
+        _items[index].quantity = quantity;
+      }
+      notifyListeners();
+    }
+  }
+}
